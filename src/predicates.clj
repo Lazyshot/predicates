89b71ca65e9c641a6ef854cd97aa3d2cffbcd61a
Bryan Peterson <lazyshot@gmail.com>
2015-01-02 23:36:21 -0500

    yay primes

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,42 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [k] (contains? a-set k)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [v] (and (pred1 v) (pred2 v))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [v] (or (pred1 v) (pred2 v))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (fn [award] (has-award? book award)) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (first (filter boolean (map pred a-seq))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (= (count a-seq) (count (filter boolean (map pred a-seq)))))
 
 (defn prime? [n]
-  :-)
+  (let [pred (fn [x] (= (mod n x) 0))]
+    (not (some pred (range 2 n)))))
 ;^^
